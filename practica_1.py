fedf680a3c688013162ab04e5e84035ab62d83ef
Jongarde <jon.gardeazabal@opendeusto.es>
2023-10-10 13:04:58 +0200

    nube de puntos hecha

--- practica_1.py
+++ practica_1.py
@@ -1,4 +1,24 @@
 import cv2
 import numpy as np
 
+point_collection = []
 
+def draw_point_line(event, x, y, flags, param):
+	global img
+
+	if event == cv2.EVENT_LBUTTONDOWN:
+		point_collection.append((x, y))
+		cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+		tam = len(point_collection)
+		if(tam > 1):
+			cv2.line(img, point_collection[tam-2], point_collection[tam-1], (0, 0, 255), 3) 
+		cv2.imshow('MyWindow', img)
+
+img = cv2.imread("Euros.jpg")
+
+cv2.namedWindow('MyWindow')
+cv2.setMouseCallback('MyWindow', draw_point_line)
+
+cv2.imshow('MyWindow', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
