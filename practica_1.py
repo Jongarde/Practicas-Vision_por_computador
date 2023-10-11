b0118c051f04c9409a2d57e5ade99a1df1e1751e
EnekoEguiguren <eneko.eguiguren@opendeusto.es>
2023-10-11 11:49:03 +0200

    crop creado, falta solucionar pequeños errores

--- practica_1.py
+++ practica_1.py
@@ -5,7 +5,6 @@ point_collection = []
 
 def draw_point_line(event, x, y, flags, param):
 	global img
-
 	if event == cv2.EVENT_LBUTTONDOWN:
 		point_collection.append((x, y))
 		cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
@@ -14,11 +13,27 @@ def draw_point_line(event, x, y, flags, param):
 			cv2.line(img, point_collection[tam-2], point_collection[tam-1], (0, 0, 255), 3) 
 		cv2.imshow('MyWindow', img)
 
+def crop_selection(img, point_collection):
+	points = np.array(point_collection)
+	if len(point_collection) == 5:
+		top_left_point = points.min(axis=0)
+		bottom_right_point = points.max(axis=0)
+		print(point_collection)
+		print(top_left_point)
+		print(bottom_right_point)
+		cropped_img = img[top_left_point[1]:bottom_right_point[1],top_left_point[0]:bottom_right_point[0]]
+		cv2.imshow("crop",cropped_img)
+	else:
+		print("You have to select 5 points!")
+
 img = cv2.imread("Euros.jpg")
 
 cv2.namedWindow('MyWindow')
 cv2.setMouseCallback('MyWindow', draw_point_line)
-
+key = cv2.waitKey()
+if key == ord('s') or key == 'S':
+	crop_selection(img,point_collection)
 cv2.imshow('MyWindow', img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+exit_key = cv2.waitKey()
+if key == 27:
+	cv2.destroyAllWindows()
