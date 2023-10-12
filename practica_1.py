5d8caea0b18531d2b81c2f2df47d8aab6a7f483c
Jongarde <jon.gardeazabal@opendeusto.es>
2023-10-12 19:01:24 +0200

    Posibilidad de dibujar más de una región por ejecución

--- practica_1.py
+++ practica_1.py
@@ -2,33 +2,37 @@ import cv2
 import numpy as np
 
 point_collection = []
+region_collection = []
 
 def draw_point_line(event, x, y, flags, param):
-	global img
-	if event == cv2.EVENT_LBUTTONDOWN:
+	global img, point_collection
+	if event == cv2.EVENT_LBUTTONDOWN:	
 		point_collection.append((x, y))
 		cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
 		tam = len(point_collection)
-		if(tam > 1 and tam < 4):
-			cv2.line(img, point_collection[tam-2], point_collection[tam-1], (0, 0, 255), 3) 
-		elif(tam == 4):
-			cv2.line(img, point_collection[tam-2], point_collection[tam-1], (0, 0, 255), 3) 
-			cv2.line(img, point_collection[tam-1],point_collection[0], (0, 0, 255), 3)
-			
+		if(tam > 1):
+			if(tam%4 == 0):
+				cv2.line(img, point_collection[tam-2], point_collection[tam-1], (0, 0, 255), 3) 
+				cv2.line(img, point_collection[tam-1],point_collection[tam-4], (0, 0, 255), 3)
+				region_collection.append(point_collection)
+				point_collection = []
+			elif(tam%4 == 1):
+				pass
+			else:
+				cv2.line(img, point_collection[tam-2], point_collection[tam-1], (0, 0, 255), 3) 			
 		cv2.imshow('MyWindow', img)
 
 def crop_selection(img, point_collection):
-	points = np.array(point_collection)
-	if len(point_collection) == 4:
-		top_left_point = points.min(axis=0)
-		bottom_right_point = points.max(axis=0)
-		print(point_collection)
-		print(top_left_point)
-		print(bottom_right_point)
-		cropped_img = img[top_left_point[1]:bottom_right_point[1],top_left_point[0]:bottom_right_point[0]]
-		cv2.imshow("crop",cropped_img)
-	else:
-		print("You have to select 5 points!")
+	for i, region in enumerate(region_collection):
+		if len(region) == 4:
+			points = np.array(region)
+			top_left_point = points.min(axis=0)
+			bottom_right_point = points.max(axis=0)
+			cropped_img = img[top_left_point[1]:bottom_right_point[1],top_left_point[0]:bottom_right_point[0]]
+			cv2.imwrite("crop" + str(i) + ".jpg", cropped_img)
+			#cv2.imshow("crop",cropped_img)
+		else:
+			print("You have to select 4 points for each region!")
 
 img = cv2.imread("Euros.jpg")
 
